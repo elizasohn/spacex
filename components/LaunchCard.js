d80bfdfe964e8bbe8aabd62e85d7cb37a3749baf
Eliza Sohn <eliza.sohn@gmail.com>
2021-02-07 22:41:13 -0800

    extract styles into stylesheet

--- components/LaunchCard.js
+++ components/LaunchCard.js
@@ -1,21 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, StyleSheet, TouchableOpacity, Text, Image } from 'react-native';
 
 export default function LaunchCard(props) {
 
-    console.log('card props', props.launch.image, props.launch.name);
-
     return (
-        <TouchableOpacity style={{ flexDirection: 'row', marginVertical: 10, height: 125, }}>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Image style={{ width: 100, height: 100 }} source={{ uri: props.launch.image }}/>
+        <TouchableOpacity style={styles.cardContainer}>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={{ uri: props.launch.image }}/>
             </View>
-            <View style={{ flex: 2, justifyContent: 'center', marginRight: 15, marginLeft: 5 }}>
-                <Text style={{ color: 'black', fontSize: 14}}> {props.launch.name}</Text>
-                <Text numberOfLines={5} style={{ fontSize: 12 }}>{props.launch.details}</Text>
+            <View style={styles.infoContainer}>
+                <Text style={styles.launchName}>{props.launch.name}</Text>
+                <Text numberOfLines={5} style={styles.details}>{props.launch.details}</Text>
             </View>
-
         </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        flexDirection: 'row', 
+        marginVertical: 5, 
+        height: 100
+    },
+    imageContainer: { 
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+    },
+    image: { 
+        width: 80, 
+        height: 80 
+    },
+    infoContainer: { 
+        flex: 2, 
+        justifyContent: 'center', 
+        marginRight: 15, 
+        marginLeft: 5 
+    },
+    launchName: { 
+        color: 'black', 
+        fontSize: 14 
+    },
+    details: {
+        fontSize: 12 
+    }
+})
 
