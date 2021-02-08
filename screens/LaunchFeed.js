68365fbe5f834743ae80859b93fb6f784a65a636
Eliza Sohn <eliza.sohn@gmail.com>
2021-02-07 18:43:22 -0800

    write out API call, pass data to LaunchList component

--- screens/LaunchFeed.js
+++ screens/LaunchFeed.js
@@ -1,15 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
-import SpaceList from '../components/SpaceList';
+import LaunchList from '../components/LaunchList';
 
-export default function SpaceFeed() {
+export default function LaunchFeed() {
+    const [launches, setLaunches] = useState([]);
+
+    useEffect(() => {
+        fetchLaunches();
+    }, [])
+
+    const fetchLaunches = async () => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+        await fetch("https://api.spacexdata.com/v3/launches", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                let launchData = result.map(launch => {
+                    let info = {
+                        "name": launch.mission_name,
+                        "details": launch.details,
+                        "image": launch.links.mission_patch
+                    }
+                    return info;
+                })
+                setLaunches(launchData);  
+            })
+            .catch(error => console.log('error', error));
+    }
 
     return (
         <View style={{ flex: 1 }}>
             <View style={{ flex: 1 }}>
-                <SpaceList
-                    // data={feedPosts} 
+                <LaunchList
+                    data={launches}
                 />
             </View>
         </View>
     );
 }
+
