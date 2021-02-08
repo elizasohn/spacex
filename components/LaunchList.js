a60a035e8dd826b966f1b9914fa520b7d9d2d379
Eliza Sohn <eliza.sohn@gmail.com>
2021-02-08 02:28:00 -0800

    extract styles, clean up imports

--- components/LaunchList.js
+++ components/LaunchList.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { View, FlatList, Text, TouchableHighlight, Dimensions } from 'react-native';
-
+import { View, StyleSheet, FlatList, Text, TouchableHighlight } from 'react-native';
 //components
 import LaunchCard from './LaunchCard';
 
 export default function LaunchList(props) {
+
     useEffect(() => {
         setPageData(onPage1)
     }, [props])
 
+    //Section out data into 6 items for each page with 5 pages, 
+    //set first page to first 6 items
     const [pageData, setPageData ] = useState(props.data.slice(0,6));
     
     const onPage1 = () => {
@@ -31,62 +33,50 @@ export default function LaunchList(props) {
         setPageData(props.data.slice(24, 30))
     };
 
-
-
     return (
+        
     <View style={{ flex: 1 }}>
         <FlatList
             scrollEnabled={false}
             style={{ flex: 1 }}
             data={pageData}
-            renderItem={({ item }) => 
+            renderItem={({ item, index }) => 
                 <LaunchCard 
                     launch={item}
-                />}
+                    key={index.toString()}
+                />
+            }
             keyExtractor={(item, index) => item.id}
             ItemSeparatorComponent={FlatListItemSeparator}
             ListFooterComponent={
-                <ListFooter 
-                    onPage1={()=> onPage1()}
+                <ListFooter
+                    onPage1={() => onPage1()}
                     onPage2={() => onPage2()}
                     onPage3={() => onPage3()}
                     onPage4={() => onPage4()}
                     onPage5={() => onPage5()}
-                    />}
+                />
+            }
         />
     </View>
     )
-
 }
 
 function ListFooter(props) {
     return (
-        <View style={{ height: 40, flex: 1, flexDirection: 'row' }}>
-            <View style={{ position: 'absolute', left: 0, right: 0, alignSelf: 'center', flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-                <Text onPress={props.onPage1} style={{ fontSize: 14, fontWeight: 'bold', color: 'black'}}>  1  </Text>
-                <Text onPress={props.onPage2}>  2  </Text>
-                <Text onPress={props.onPage3}>  3  </Text>
-                <Text onPress={props.onPage4}>  4  </Text>
-                <Text onPress={props.onPage5}>  5  </Text>
+        <View style={styles.footer}>
+            <View style={styles.numbersContainer}>
+                <Text onPress={props.onPage1} style={styles.numbers}>  1  </Text>
+                <Text onPress={props.onPage2} style={styles.numbers}>  2  </Text>
+                <Text onPress={props.onPage3} style={styles.numbers}>  3  </Text>
+                <Text onPress={props.onPage4} style={styles.numbers}>  4  </Text>
+                <Text onPress={props.onPage5} style={styles.numbers}>  5  </Text>
             </View>
             <TouchableHighlight
-                style={{
-                    left: 300,
-                    right: 20,
-                    borderRadius: 40,
-                    backgroundColor: 'lightgray',
-                    width: 40,
-                    height: 40,
-                    borderColor: 'black',
-                    borderWidth: 1,
-                    alignItems: 'center',
-                    alignSelf: 'flex-end',
-                    marginRight: 10,
-                }}
+                style={styles.addLaunch}
                 underlayColor='#ccc'
-                onPress={() => alert('Yaay!')}
-            >
-                <Text style={{ fontSize: 30, lineHeight: 35, color: 'black' }}> + </Text>
+                onPress={() => alert('Needs navigation dependency')}>
+                <Text style={styles.plus}> + </Text>
             </TouchableHighlight>
         </View>
     );
@@ -95,11 +85,52 @@ function ListFooter(props) {
 function FlatListItemSeparator() {
     return (
         <View
-            style={{
-                height: 0.5,
-                width: "100%",
-                backgroundColor: 'rgba(179, 179, 179, 0.4)',
-            }}
+            style={styles.separator}
         />
     );
 };
+
+const styles = StyleSheet.create({
+    footer: { 
+        height: 40, 
+        flex: 1, 
+        flexDirection: 'row'
+    },
+    addLaunch: {
+        left: 300,
+        right: 20,
+        borderRadius: 40,
+        backgroundColor: 'lightgray',
+        width: 40,
+        height: 40,
+        borderColor: 'black',
+        borderWidth: 1,
+        alignItems: 'center',
+        alignSelf: 'flex-end',
+        marginRight: 10,
+    }, 
+    numbers: { 
+        fontSize: 14, 
+        fontWeight: 'bold', 
+        color: 'black' 
+    }, 
+    numbersContainer: { 
+        position: 'absolute', 
+        left: 0, 
+        right: 0, 
+        alignSelf: 'center', 
+        flex: 1, 
+        flexDirection: 'row', 
+        justifyContent: 'center' 
+    }, 
+    plus: { 
+        fontSize: 30, 
+        lineHeight: 35, 
+        color: 'black' 
+    }, 
+    separator: {
+        height: 0.5,
+        width: "100%",
+        backgroundColor: 'rgba(179, 179, 179, 0.4)',
+    }
+})
--- screens/LaunchFeed.js
+++ screens/LaunchFeed.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import LaunchList from '../components/LaunchList';
 import useLaunchData from '../customHooks/useLaunchData';
 
 export default function LaunchFeed() {
     const [launches] = useLaunchData()
-    console.log('launches', launches);
-    
+
     return (
         <View style={{ flex: 1 }}>
                 <LaunchList
