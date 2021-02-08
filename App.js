5f0e9fa80728d2bb13edf279729e0c5cd2759bae
Eliza Sohn <eliza.sohn@gmail.com>
2021-02-08 02:31:32 -0800

    import CreateLaunch and comment out screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LaunchFeed from './screens/LaunchFeed';
+import CreateLaunch from './screens/CreateLaunch';
 
 export default function App() {
 
   return (
-    <LaunchFeed />
+    <LaunchFeed/>
+    // <CreateLaunch/> 
   );
 }
 
